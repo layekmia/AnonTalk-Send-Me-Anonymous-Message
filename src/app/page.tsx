80f32529cd4903c14c2c860611247691df8f8d5e
Layek Miah <layekofficial63@gmail.com>
2025-11-01 14:09:53 +0600

    try to fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Documentation
+            Documentation of Nextjs
           </a>
         </div>
       </main>
