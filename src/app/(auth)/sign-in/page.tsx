9bd18c3f496d0a851520eea576bdddc28f184d13
Layek Miah <layekofficial63@gmail.com>
2025-11-01 12:32:15 +0600

    create sign-inpage

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+
+import SubmitButton from "@/components/SubmitButton";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { signInSchema } from "@/schemas/signInSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import z from "zod";
+
+export default function Page() {
+  const router = useRouter();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSubmitting(true);
+
+    try {
+      const result = await signIn("domain-login", {
+        redirect: false, // handle redirect manually
+        identifier: data.identifier.trim(),
+        password: data.password,
+      });
+
+      // Handle NextAuth errors (result.error is string if login fails)
+      if (result?.error) {
+        toast.error(result.error);
+        return;
+      }
+
+      // Handle successful login
+      toast.success("Successfully logged in!");
+      router.replace("/dashboard");
+    } catch (error) {
+      console.error("Login error:", error);
+      toast.error("Something went wrong. Please try again.");
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            verify Your Account
+          </h1>
+          <p className="mb-4">Enter the verification code sent to your email</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email or username" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <SubmitButton label="singing..." isSubmitting={isSubmitting}>
+              Sign In
+            </SubmitButton>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
